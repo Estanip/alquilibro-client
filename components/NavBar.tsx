e1b41c94f52306957beb620d0192005d4a812d06
NaniBM <bazanmurua@gmail.com>
2021-11-09 12:02:39 -0300

    splash screen + iconos de la app

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -34,25 +34,25 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'center',
-        padding: '16px',
-        width: '411px',
-        height: '56px',
-        left: '0px',
-        top: '24px',
+        padding: 16,
+        width: 411,
+        height: 56,
+        left: 0,
+        top: 24,
         backgroundColor: '#7ECA9C'
     },
     textContainer: {
         position: 'absolute',
-        width: '189px',
-        height: '24px',
-        left: '86px',
-        top: '16px',
+        width: 189,
+        height: 24,
+        left: 86,
+        top: 16,
         backgroundColor: '#7ECA9C'
     },
     title: {
         fontFamily: 'Roboto',
         fontStyle: 'normal',
-        fontWeight: '500',
+        fontWeight: 'bold',
         fontSize: 20,
         lineHeight: 24,
         letterSpacing: 0.15,
@@ -60,29 +60,29 @@ const styles = StyleSheet.create({
     },
     iconContainer: {
         position: 'absolute',
-        width: '37px',
-        height: '37px',
-        left: '16px',
-        top: '9.5px'
+        width: 37,
+        height: 37,
+        left: 16,
+        top: 9.5
     },
     alquilibroIcon: {
-        width: '24px',
-        height: '25px',
-        left: '22px',
-        top: '15px'
+        width: 24,
+        height: 25,
+        left: 22,
+        top: 15
     },
     shareContainer: {
         position: 'absolute',
-        width: '24.12px',
-        height: '24px',
-        right: '21.88px',
-        top: '16px'
+        width: 24.12,
+        height: 24,
+        right: 21.88,
+        top: 16
     },
     notifyContainer: {
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'flex-start',
-        padding: '0px',
+        padding: 0,
         position: 'absolute',
         left: '74.94%',
         right: '19.22%',
