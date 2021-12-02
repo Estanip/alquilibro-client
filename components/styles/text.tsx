2492b79043bad391f9437dc7eb2b4b8c7d9d4b50
NaniBM <bazanmurua@gmail.com>
2021-12-02 17:49:35 -0300

    boton con icono

--- components/styles/text.tsx
+++ components/styles/text.tsx
@@ -1,37 +1,34 @@
 import { StyleSheet } from "react-native";
 
 const textStyle = StyleSheet.create({
-
-    title: {
-        fontFamily: 'Roboto',
-        fontStyle: 'normal',
-        fontWeight: 'bold',
-        fontSize: 24,
-        lineHeight: 24,
-        textAlign: 'center',
-        color: '#1C1427',
-        letterSpacing: 0.15
-    },
-    buttonTextBlack: {
-        fontFamily: "Roboto",
-        fontStyle: "normal",
-        fontWeight: "500",
-        fontSize: 14,
-        lineHeight: 22,
-        letterSpacing: 1.25,
-        color: "#1C1427",
-    },
-    buttonTextWhite: {
-        fontFamily: "Roboto",
-        fontStyle: "normal",
-        fontWeight: "500",
-        fontSize: 14,
-        lineHeight: 22,
-        letterSpacing: 1.25,
-        color: "#FFFFFF",
-    }
-
+  title: {
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: 24,
+    lineHeight: 24,
+    textAlign: "center",
+    color: "#1C1427",
+    letterSpacing: 0.15,
+  },
+  buttonTextBlack: {
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: 14,
+    lineHeight: 22,
+    letterSpacing: 1.25,
+    color: "#1C1427",
+  },
+  buttonTextWhite: {
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: 14,
+    lineHeight: 22,
+    letterSpacing: 1.25,
+    color: "#FFFFFF",
+  },
 });
 
 export default textStyle;
-
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,63 +1,70 @@
-import * as React from 'react';
-import { StyleSheet, View, Text, ScrollView } from 'react-native';
-import { RootTabScreenProps } from '../types';
+import * as React from "react";
+import { StyleSheet, View, Text, ScrollView } from "react-native";
+import { RootTabScreenProps } from "../types";
 
-import textStyle from '../components/styles/text';
-import HomeCards from '../components/HomeCards';
-import SearchInputHome from '../components/SearchInputHome';
-import UploadBookButton from '../components/UploadBookButton';
+import textStyle from "../components/styles/text";
+import HomeCards from "../components/HomeCards";
+import SearchInputHome from "../components/SearchInputHome";
+import buttonStyle from "../components/styles/button";
+import ButtonText from "../components/ButtonText";
+import { useNavigation } from "@react-navigation/native";
 
-export default function Home({ navigation }: RootTabScreenProps<'Home'>) {
+export default function Home({ navigation }: RootTabScreenProps<"Home">) {
+  const handleOnClick = () => {
+    navigation.navigate("Upload");
+  };
 
-    return (
-        <ScrollView
-            style={{backgroundColor: '#FFF'}}
-        >
-            <View style={styles.homeContainer}>
-                <View style={styles.titleOneContainer}>
-                    <Text style={textStyle.title}>
-                        ¿Buscas un libro en especial?
-                    </Text>
-                </View>
-                <SearchInputHome />
-                <View style={styles.titleTwoContainer}>
-                    <Text style={textStyle.title}>
-                        ¿No sabés qué leer?
-                    </Text>
-                </View>
-                <HomeCards />
-                <View style={styles.titleThreeContainer}>
-                    <Text style={textStyle.title}>
-                        ¿Querés alquilar tus libros?
-                    </Text>
-                </View>
-                <UploadBookButton />
-            </View>
-        </ScrollView>
-    )
-};
+  return (
+    <ScrollView style={{ backgroundColor: "#FFF" }}>
+      <View style={styles.homeContainer}>
+        <View style={styles.titleOneContainer}>
+          <Text style={textStyle.title}>¿Buscas un libro en especial?</Text>
+        </View>
+        <SearchInputHome />
+        <View style={styles.titleTwoContainer}>
+          <Text style={textStyle.title}>¿No sabés qué leer?</Text>
+        </View>
+        <HomeCards />
+        <View style={styles.titleThreeContainer}>
+          <Text style={textStyle.title}>¿Querés alquilar tus libros?</Text>
+          <ButtonText
+            title={"SUBIR LIBRO"}
+            textStyle={textStyle.buttonTextBlack}
+            styles={buttonStyle.white}
+            handleSubmit={handleOnClick}
+            disabled={false}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
 
 const styles = StyleSheet.create({
-    homeContainer: {
-        width: '100%',
-        maxHeight: 730,
-        minHeight: 640,
-        backgroundColor: '#FFF'
-    },
-    titleOneContainer: {
-        width: '100%',
-        alignSelf: 'center',
-        top: '10%'
-    },
-    titleTwoContainer: {
-        width: '100%',
-        alignSelf: 'center',
-        height: 24,
-        top: '25%'
-    },
-   titleThreeContainer: {
-        width: '100%',
-        alignSelf: 'center',
-        top: '45%'
-    }
+  homeContainer: {
+    width: "100%",
+    maxHeight: 730,
+    minHeight: 640,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+  },
+  titleOneContainer: {
+    width: "100%",
+    top: "10%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 10,
+  },
+  titleTwoContainer: {
+    width: "100%",
+    height: 24,
+    top: "25%",
+  },
+  titleThreeContainer: {
+    width: "100%",
+    top: "45%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 10,
+  },
 });
